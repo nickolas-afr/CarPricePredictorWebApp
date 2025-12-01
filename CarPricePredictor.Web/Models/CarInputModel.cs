5f1a9e740cbef8855028f8d2e2f3b588955cac63
nickolas-afr <nikolas.afrem15@gmail.com>
2025-12-01 17:03:11 +0200

    added form validation

--- CarPricePredictor.Web/Models/CarInputModel.cs
+++ CarPricePredictor.Web/Models/CarInputModel.cs
@@ -1,14 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CarPricePredictor.Web.Models;
 
 public class CarInputModel
 {
+    [Required(ErrorMessage = "Brand is required")]
     public string Make { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Model is required")]
     public string Model { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Production Year is required")]
+    [Range(1990, 2025, ErrorMessage = "Production Year must be between 1900 and 2025")]
+    public int Year { get; set; }
+
+    [Required(ErrorMessage = "Mileage is required")]
+    [Range(0, 1000000, ErrorMessage = "Mileage must be between 0 and 1,000,000 km")]
     public float Mileage { get; set; }
+
     public string Fuel { get; set; } = string.Empty;
     public string Gear { get; set; } = string.Empty;
-    public float OfferType { get; set; }
-    public float Price { get; set; }
     public float Hp { get; set; }
-    public int Year { get; set; }
+
+    [Required(ErrorMessage = "Offer Type is required")]
+    public float OfferType { get; set; }
+
+    [Required(ErrorMessage = "Asking Price is required")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "Asking Price must be greater than 0")]
+    public float Price { get; set; }
 }
